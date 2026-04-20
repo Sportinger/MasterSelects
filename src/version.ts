58f1b9128c3752bc2742e7ed81c569689f08e314
Sportinger <roman.kuskowski@googlemail.com>
2026-04-20 11:17:19 +0200

    chore: bump version to 1.5.6

--- src/version.ts
+++ src/version.ts
@@ -1,6 +1,6 @@
 // App version
 // Format: MAJOR.MINOR.PATCH
-export const APP_VERSION = '1.5.5';
+export const APP_VERSION = '1.5.6';
 
 export interface ChangelogNotice {
   type: 'info' | 'warning' | 'success' | 'danger';
